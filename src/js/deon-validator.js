a359ab892e9f7046e4999bba2465d44420c234f9
ddesir <d.desir@gmail.com>
2021-01-15 15:55:21 -0500

    initial validators

---
+++ src/js/deon-validator.js
@@ -0,0 +1,25 @@
+// assume all fields must have at least 3 characters
+// assume all fields cannot be more than 20 characters
+// Fields cannot contain numbers
+
+function validate_name(value){
+    const RENUM = /[0-9]/;
+
+    if(value.length < 3){
+	return 'Must be at least 3 characters long';
+    }
+
+    if(value.length > 20){
+	return 'Cannot be more than 20 characters long'
+    }
+    
+    if(RENUM.test(value)) {
+        return `"${value}" is not a valid input. Names can not contain numbers.`;
+    }
+    
+    // implied else
+    return null;
+}
+
+
+export {validate_name};
