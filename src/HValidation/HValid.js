2b427919ba388a69cf3f5ad561d2c39f305f2739
Henotis <Henryli@Mec.science>
2021-01-15 20:49:38 -0500

    set another parameter for the input value of username, it now requires an email

--- src/HValidation/HValid.js
+++ src/HValidation/HValid.js
@@ -5,25 +5,29 @@
 // otherwise a string with a description of the error
 
 function usernames(value) {
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
     if (value.length < 3) {
         return 'Must be at least 3 characters long';
     }
     if (value.length > 20) {
         return 'Cannot be more than 20 characters long'
     }
-
-    // implied else
-    return '';
+    if (value.match(mailformat)) {
+        return '';
+    }
+    else{
+        return 'Not a valid email'
+    }
 }
 
 function passwords(value) {
     let symbolCounter = 0;
     for (let i = 0; i < value.length; i++) {
         if (value[i] == '$') {
-            symbolCounter+= 1;
+            symbolCounter += 1;
         }
     }
-
 
     if (value.length < 3) {
         return 'Must be at least 3 characters long';
@@ -38,7 +42,6 @@ function passwords(value) {
     // implied else
     return '';
 }
-
 
 
 export {
